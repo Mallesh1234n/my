3bfb7dc7879b7999a69b835152bbe7cce34ba4ab
Mallesh.N <154885534+Mallesh1234n@users.noreply.github.com>
2023-12-28 10:54:09 +0530

    Add files via upload

---
+++ my-portfoil/my-portfoile/javascript/custom.js
@@ -0,0 +1,34 @@
+$('.numbers').slick({
+  // infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // arrows:false,
+  dots:true,
+  arrows:false,
+});
+
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
+
+    $('#cmd').click(function () {
+        doc.fromHTML($('#content').html(), 15, 15, {
+            'width': 170,
+                'elementHandlers': specialElementHandlers
+        });
+        doc.save('sample-file.pdf');
+    });
+
+  
+    
